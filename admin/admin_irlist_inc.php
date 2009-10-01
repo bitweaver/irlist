c4649e55a249654daefb75fa78697ddc08095cca
wjames5 <will@tekimaki.com>
2009-10-01 14:17:00 +0000

    remove ref to non-existant copyright.txt file

--- admin/admin_irlist_inc.php
+++ admin/admin_irlist_inc.php
@@ -1,9 +1,9 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_irlist/admin/admin_irlist_inc.php,v 1.4 2009/10/01 13:45:42 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_irlist/admin/admin_irlist_inc.php,v 1.5 2009/10/01 14:17:00 wjames5 Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 include_once( IRLIST_PKG_PATH.'IRList.php' );
